01225c2a40b125ab3e13c447257ebe4e110447a2
jeremie2k1 <vietngaitmo@gmail.com>
2022-10-24 03:00:02 +0300

    Lab 2

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Closure",
+        artist: "Hayd",
+        img: "music_1",
+        src: "music_1"
+    },
+    {
+        name: "Head In The Clouds",
+        artist: "Hayd",
+        img: "music_2",
+        src: "music_2"
+    },
+    {
+        name: "Legend Never Die",
+        artist: "Riot",
+        img: "music_3",
+        src: "music_3"
+    },
+    {
+        name: "Không sao mà, em đây rồi",
+        artist: "Suni Hạ Linh",
+        img: "music_4",
+        src: "music_4"
+    },
+    {
+        name: "Vô Cực Nhớ Em",
+        artist: "Lil Zpoet",
+        img: "music_5",
+        src: "music_5"
+    }
+];
